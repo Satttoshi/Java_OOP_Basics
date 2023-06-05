ebada772ece9d4246e20cdd0c4176eb251c0585e
Josh <joshuastolle445@gmail.com>
2023-06-05 15:24:41 +0200

    add equals override and hashcodes

--- src/main/java/org/model/Main.java
+++ src/main/java/org/model/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Student student1 = new Student();
-        student1.setName("John");
-        student1.setAge(20);
-        student1.setPoints(100);
+        student1.setName("Peter");
+        student1.setAge(21);
+        student1.setPoints(69);
 
         System.out.println("Name: " + student1.getName());
         System.out.println("Age: " + student1.getAge());
@@ -17,5 +17,10 @@ public class Main {
 
         Student student2 = new Student("Peter", 21, 69);
         System.out.println(student2);
+
+        // Try out overwrited equals Method
+        System.out.println("Are the instances equal?: " + student1.equals(student2));
+
+
     }
 }
