6e30895027111d1651f978ce12fba14dc00cfef8
Josh <joshuastolle445@gmail.com>
2023-06-06 01:52:12 +0200

    remove unused imports

--- src/test/java/org/model/TestStudentDB.java
+++ src/test/java/org/model/TestStudentDB.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 class TestStudentDB {
 
     private static final Student[] STUDENTS = new Student[]{
